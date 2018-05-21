258a06a1cf79d1676d00c5241afbdb2baf0e82e6
Konrad <konmos@users.noreply.github.com>
2018-05-21 16:40:30 +0100

    Minor reworks

--- mbot/plugins/reddit.py
+++ mbot/plugins/reddit.py
@@ -4,117 +4,133 @@ import random
 import praw
 import discord
 from prawcore import exceptions
+from praw.exceptions import ClientException
 
-from ..config import Config
 from ..plugin import BasePlugin
 from ..command import command
 from ..utils import long_running_task
 
 
-_cfg = Config(os.environ['mbot_config'])
-if _cfg.plugin_data.get('reddit', {}).get('client_id', '') and \
-    _cfg.plugin_data.get('reddit', {}).get('client_secret', ''):
+class Reddit(BasePlugin):
+    def __init__(self, mbot):
+        super().__init__(mbot)
 
-    class Reddit(BasePlugin):
-        def __init__(self, mbot):
-            super().__init__(mbot)
+        self.client_id = self.mbot.config.plugin_data.get('reddit', {}).get('client_id')
+        self.client_secret = self.mbot.config.plugin_data.get('reddit', {}).get('client_secret')
 
-            self.client_id = self.mbot.config.plugin_data.get('reddit', {}).get('client_id', '')
-            self.client_secret = self.mbot.config.plugin_data.get('reddit', {}).get('client_secret', '')
+        self.user_agent = f'{os.name}:markobot.xyz:{self.client_id} (by /u/Kojak2)'
 
-            self.user_agent = f'{os.name}:github.com/konmos/MarkoBot:v0-{self.client_id[:6]} (/u/markothebot)'
-
+        try:
             self.reddit = praw.Reddit(
                 client_id=self.client_id,
                 client_secret=self.client_secret,
                 user_agent=self.user_agent
             )
+        except ClientException:
+            self.reddit = None
 
-        @long_running_task()
-        def _get_submissions(self, subreddit, sort=None, limit=25):
-            try:
-                reddit = self.reddit.subreddit(subreddit)
+    async def _config_check(self, channel=None):
+        if self.client_id and self.client_secret and self.reddit:
+            return True
 
-                submissions = {
-                    'hot': reddit.hot,
-                    'controversial': reddit.controversial,
-                    'gilded': reddit.gilded,
-                    'new': reddit.new,
-                    'rising': reddit.rising,
-                    'top': reddit.top
-                }.get(sort, reddit.new)
+        if channel:
+            await self.mbot.send_message(
+                channel, '*Well... This shouldn\'t happen. I am missing my reddit config.* :cry:'
+            )
 
-                posts = []
+        return False
 
-                for post in submissions(limit=limit):
-                    posts.append(post)
-            except (exceptions.RequestException, exceptions.ResponseException):
-                posts = []
+    @long_running_task()
+    def _get_submissions(self, subreddit, sort=None, limit=25):
+        try:
+            reddit = self.reddit.subreddit(subreddit)
 
-            return posts
+            submissions = {
+                'hot': reddit.hot,
+                'controversial': reddit.controversial,
+                'gilded': reddit.gilded,
+                'new': reddit.new,
+                'rising': reddit.rising,
+                'top': reddit.top
+            }.get(sort, reddit.new)
 
-        async def _grab_image(self, subreddit, sort=None, _message=None):
-            images = []
-            submissions = await self._get_submissions(subreddit, sort, _message=_message)
-
-            for post in submissions:
-                try:
-                    if post.url.endswith('.jpg') or post.url.endswith('.png'):
-                        images.append((post.title, post.url))
-                except AttributeError:
-                    pass
-
-            if images:
-                return random.choice(images)
-
-            return []
-
-        async def _grab_post(self, subreddit, sort=None, _message=None):
             posts = []
-            submissions = await self._get_submissions(subreddit, sort, _message=_message)
 
-            for post in submissions:
-                try:
-                    if post.is_self:
-                        posts.append((post.title, post.selftext, post.author, post.score, post.id))
-                except AttributeError:
-                    pass
+            for post in submissions(limit=limit):
+                posts.append(post)
+        except (exceptions.RequestException, exceptions.ResponseException):
+            posts = []
 
-            if posts:
-                return random.choice(posts)
+        return posts
 
-            return []
+    async def _grab_image(self, subreddit, sort=None, _message=None):
+        images = []
+        submissions = await self._get_submissions(subreddit, sort, _message=_message)
 
-        @command(regex='^image (.*?)(?: (.*?))?$', usage='image <subreddit>',
-                 description='grab a random image from a subreddit', cooldown=5)
-        async def image(self, message, subreddit, sort=None):
-            im = await self._grab_image(subreddit, sort, _message=message)
+        for post in submissions:
+            try:
+                if post.url.endswith('.jpg') or post.url.endswith('.png'):
+                    images.append((post.title, post.url))
+            except AttributeError:
+                pass
 
-            if im:
-                await self.mbot.send_file(message.channel, im[1], content=f'**{im[0]}**')
-            else:
-                await self.mbot.send_message(
-                    message.channel,
-                    f'**{message.author.mention} I couldn\'t find anything...** :cry:'
-                )
+        if images:
+            return random.choice(images)
 
-        @command(regex='^reddit (.*?)(?: (.*?))?$', usage='reddit <subreddit>',
-                 description='grab a random post from a subreddit', cooldown=5)
-        async def reddit(self, message, subreddit, sort=None):
-            post = await self._grab_post(subreddit, sort, _message=message)
+        return []
 
-            if post:
-                embed = discord.Embed(
-                    title=f'[{post[3]}] {post[0]} ({post[4]})',
-                    description=f'{post[1][:1024]}\n...',
-                    colour=0x0277bd
-                )
+    async def _grab_post(self, subreddit, sort=None, _message=None):
+        posts = []
+        submissions = await self._get_submissions(subreddit, sort, _message=_message)
 
-                embed.set_footer(text=f'/u/{post[2]}')
+        for post in submissions:
+            try:
+                if post.is_self:
+                    posts.append((post.title, post.selftext, post.author, post.score, post.id))
+            except AttributeError:
+                pass
 
-                await self.mbot.send_message(message.channel, embed=embed)
-            else:
-                await self.mbot.send_message(
-                    message.channel,
-                    f'**{message.author.mention} I couldn\'t find anything...** :cry:'
-                )
+        if posts:
+            return random.choice(posts)
+
+        return []
+
+    @command(regex='^image (.*?)(?: (.*?))?$', usage='image <subreddit>',
+             description='grab a random image from a subreddit', cooldown=5)
+    async def image(self, message, subreddit, sort=None):
+        if not await self._config_check(message.channel):
+            return
+
+        im = await self._grab_image(subreddit, sort, _message=message)
+
+        if im:
+            await self.mbot.send_file(message.channel, im[1], content=f'**{im[0]}**')
+        else:
+            await self.mbot.send_message(
+                message.channel,
+                f'**{message.author.mention} I couldn\'t find anything...** :cry:'
+            )
+
+    @command(regex='^reddit (.*?)(?: (.*?))?$', usage='reddit <subreddit>',
+             description='grab a random post from a subreddit', cooldown=5)
+    async def reddit(self, message, subreddit, sort=None):
+        if not await self._config_check(message.channel):
+            return
+
+        post = await self._grab_post(subreddit, sort, _message=message)
+
+        if post:
+            embed = discord.Embed(
+                title=f'[{post[3]}] {post[0]} ({post[4]})',
+                description=f'{post[1][:1024]}\n...',
+                colour=0x0277bd
+            )
+
+            embed.set_footer(text=f'/u/{post[2]}')
+
+            await self.mbot.send_message(message.channel, embed=embed)
+        else:
+            await self.mbot.send_message(
+                message.channel,
+                f'**{message.author.mention} I couldn\'t find anything...** :cry:'
+            )
