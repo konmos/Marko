2b755b693950243ed4d700c099d14f4bffbddd31
Konrad <konmos@users.noreply.github.com>
2018-08-17 14:09:09 +0100

    Rework custom command parsing

--- mbot/plugins/cc.py
+++ mbot/plugins/cc.py
@@ -1,106 +1,217 @@
 import re
 import shlex
+import random
 import asyncio
 from copy import copy
 # noinspection PyUnresolvedReferences
 from string import whitespace
 
+from lark import Lark, Transformer
 from discord import Forbidden
 
 from ..plugin import BasePlugin
 from ..command import command
 
 
+RESERVED_CHARS = {
+    '$(semicolon)': ';',
+    '$(lbrace)': '{',
+    '$(rbrace)': '}',
+    '$(quote)': '"',
+}
+
+GRAMMAR = r'''
+    start: cmd+
+    ?cmd: speak
+         | pm
+         | block
+         | random
+         | if_stmt
+         | role
+         | perms
+         | command
+         | sleep
+         | cp_disable
+         | cp_enable
+         | gc_disable
+         | gc_enable
+         | delete
+         | nop
+
+    block: "{" "block" ":" cmd (";" cmd)+ "}"
+    if_stmt: "{" "if" ":" ESCAPED_STRING ";" IF_OP ";" ESCAPED_STRING ";" cmd ";" cmd "}"
+    speak: "{" "speak" ":" ESCAPED_STRING "}" | TEXT
+    pm: "{" "pm" ":" ESCAPED_STRING "}"
+    role: "{" "role" ":" ESCAPED_STRING "}"
+    perms: "{" "perms" ":" HEX "}"
+    command: "{" "cmd" ":" ESCAPED_STRING "}"
+    sleep: "{" "sleep" ":" INT "}"
+    cp_enable: "{" "check_perms" "}"
+    cp_disable: "{" "!check_perms" "}"
+    gc_enable: "{" "global_commands" "}"
+    gc_disable: "{" "!global_commands" "}"
+    delete: "{" "delete" "}"
+    random: "{" "random" ":" cmd (";" cmd)+ "}"
+    nop: "{" "nop" "}"
+
+    IF_OP: "eq" | "ne" | "in" | "not in" | "lt" | "gt" | "le" | "ge"
+    CHAR: "a".."z" | "A".."Z" | "0".."9" | /[!\"#$%&\'()*+,\-.\/:<=>?@[\\\]\^_`\|~]/
+    TEXT: CHAR (WS? CHAR)*
+    HEX: HEXDIGIT+
+
+    %import common.ESCAPED_STRING
+    %import common.HEXDIGIT
+    %import common.INT
+    %import common.WS
+    %ignore WS
+    '''
+
+
 class ParsingError(Exception):
     pass
+
+
+class MyTransformer(Transformer):
+    def block(self, args):
+        return ('block', *args)
+
+    def if_stmt(self, args):
+        return (
+            'if',
+            CustomCommands.subs_vars(RESERVED_CHARS, args[0].value[1:-1]),
+            args[1].value,
+            CustomCommands.subs_vars(RESERVED_CHARS, args[2].value[1:-1]),
+            *args[3:]
+        )
+
+    def speak(self, args):
+        if args[0].type == 'TEXT':
+            return 'speak', CustomCommands.subs_vars(RESERVED_CHARS, args[0].value)
+
+        return 'speak', CustomCommands.subs_vars(RESERVED_CHARS, args[0][1:-1])
+
+    def pm(self, args):
+        return 'pm', CustomCommands.subs_vars(RESERVED_CHARS, args[0][1:-1])
+
+    def role(self, args):
+        return 'role', CustomCommands.subs_vars(RESERVED_CHARS, args[0].value[1:-1])
+
+    def perms(self, args):
+        return 'perms', int(args[0], base=16)
+
+    def command(self, args):
+        return 'cmd', CustomCommands.subs_vars(RESERVED_CHARS, args[0].value[1:-1])
+
+    def sleep(self, args):
+        return 'sleep', min(int(args[0]), 10)
+
+    def cp_enable(self, *args):
+        return 'check_perms', None
+
+    def cp_disable(self, *args):
+        return '!check_perms', None
+
+    def gc_enable(self, *args):
+        return 'global_commands', None
+
+    def gc_disable(self, *args):
+        return '!global_commands', None
+
+    def delete(self, *args):
+        return 'delete', None
+
+    def random(self, args):
+        return ('random', *args)
+
+    def nop(self, *args):
+        return 'nop', None
 
 
 class CustomCommands(BasePlugin):
     def __init__(self, mbot):
         super().__init__(mbot)
 
+        self.parser = Lark(GRAMMAR, parser='lalr', transformer=MyTransformer())
         self.cc_db = self.mbot.mongo.plugin_data.custom_commands
 
-    def parse_cc(self, message, cmd_string, args):
-        tokens = []
-        lines = cmd_string.split(';')
-
-        if len(lines) > 10:
-            raise ParsingError('Maximum script size exceeded.')
-
+    @staticmethod
+    def subs_vars(replacements, string):
         # code from https://stackoverflow.com/questions/6116978/how-to-replace-multiple-substrings-of-a-string
+        rep = dict((re.escape(k), v) for k, v in replacements.items())
+        pattern = re.compile('|'.join(rep.keys()))
+        return pattern.sub(lambda m: rep[re.escape(m.group(0))], string)
+
+    def parse_cc(self, cmd_string, message, args):
         replacements = {
-            '$sep': ';',
+            '$(user.name)': message.author.name,
+            '$(user.discriminator)': message.author.discriminator,
+            '$(user.mention)': message.author.mention,
+            '$(user.avatar)': message.author.avatar_url or message.author.default_avatar_url,
+            '$(user.id)': message.author.id,
+            '$(user.created_at)': str(message.author.created_at),
+            '$(user.display_name)': message.author.display_name,
+            '$(user.joined_at)': str(message.author.joined_at),
+            '$(user.status)': str(message.author.status),
+            '$(user.game)': str(message.author.game) if message.author.game else '',
+            '$(user.nick)': message.author.nick or '',
+            '$(user.color)': str(message.author.colour),
+            '$(user.colour)': str(message.author.colour),
 
-            '$user.name': message.author.name,
-            '$user.discriminator': message.author.discriminator,
-            '$user.mention': message.author.mention,
-            '$user.avatar': message.author.avatar_url or message.author.default_avatar_url,
-            '$user.id': message.author.id,
-            '$user.created_at': str(message.author.created_at),
-            '$user.display_name': message.author.display_name,
-            '$user.joined_at': str(message.author.joined_at),
-            '$user.status': str(message.author.status),
-            '$user.game': str(message.author.game) if message.author.game else '',
-            '$user.nick': message.author.nick or '',
-            '$user.color': str(message.author.colour),
-            '$user.colour': str(message.author.colour),
+            '$(server.name)': message.server.name,
+            '$(server.id)': message.server.id,
+            '$(server.region)': str(message.server.region),
+            '$(server.afk_timeout)': str(message.server.afk_timeout),
+            '$(server.icon_url)': message.server.icon_url,
+            '$(server.splash_url)': message.server.splash_url,
+            '$(server.members)': str(message.server.member_count),
+            '$(server.created_at)': str(message.server.created_at),
 
-            '$server.name': message.server.name,
-            '$server.id': message.server.id,
-            '$server.region': str(message.server.region),
-            '$server.afk_timeout': str(message.server.afk_timeout),
-            '$server.icon_url': message.server.icon_url,
-            '$server.splash_url': message.server.splash_url,
-            '$server.members': str(message.server.member_count),
-            '$server.created_at': str(message.server.created_at),
-
-            '$channel.id': message.channel.id,
-            '$channel.name': message.channel.name,
-            '$channel.created_at': str(message.channel.created_at),
-            '$channel.mention': message.channel.mention,
-            '$channel.position': str(message.channel.position)
+            '$(channel.id)': message.channel.id,
+            '$(channel.name)': message.channel.name,
+            '$(channel.created_at)': str(message.channel.created_at),
+            '$(channel.mention)': message.channel.mention,
+            '$(channel.position)': str(message.channel.position)
         }
 
         for i, arg in enumerate(args):
-            replacements[f'${i + 1}'] = arg
-            replacements[f'$>{i + 1}'] = ' '.join(args[i:])
-            replacements[f'$<{i + 1}'] = ' '.join(args[:i])
+            replacements[f'$({i + 1})'] = arg
+            replacements[f'$(>{i + 1})'] = ' '.join(args[i:])
+            replacements[f'$(<{i + 1})'] = ' '.join(args[:i])
 
-        rep = dict((re.escape(k), v) for k, v in replacements.items())
-        pattern = re.compile('|'.join(rep.keys()))
+        cmd_string = self.subs_vars(replacements, cmd_string)
 
-        for line in lines:
-            line = line.strip()
+        try:
+            return self.parser.parse(cmd_string).children
+        except Exception as e:
+            raise ParsingError(str(e))
 
-            if not line:
-                continue
-
-            op, *args = line.split(' ', 1)
-
-            try:
-                args[0] = pattern.sub(lambda m: rep[re.escape(m.group(0))], args[0])
-
-                if op in ['speak', 'role', 'cmd', 'pm']:
-                    tokens.append((op, args[0]))
-
-                elif op == 'sleep':
-                    tokens.append((op, max(int(args[0]), 10)))
-
-                elif op == 'perms':
-                    tokens.append((op, int(args[0])))
-
-                elif op in ['delete', 'check_perms', '!check_perms', 'global_commands', '!global_commands']:
-                    tokens.append((op, None))
-            except:
-                raise ParsingError(f'Error parsing line: {repr(line)}')
-
-        return tokens
-
-    async def execute_cc(self, message, tokens):
-        env = {'check_perms': True, 'global_commands': False}
+    async def execute_cc(self, message, tokens, env=None, _depth=0):
+        env = env or {'check_perms': True, 'global_commands': False}
 
         for token in tokens:
+            if token[0] == 'if':
+                bool_map = {
+                    'eq': token[1] == token[3],
+                    'ne': token[1] != token[3],
+                    'in': token[1] in token[3],
+                    'not in': token[1] not in token[3],
+                    'lt': token[1] < token[3],
+                    'gt': token[1] > token[3],
+                    'le': token[1] <= token[3],
+                    'ge': token[1] >= token[3]
+                }
+
+                if bool_map[token[2]]:
+                    env = await self.execute_cc(message, [token[4]], env=env, _depth=_depth + 1)
+                else:
+                    env = await self.execute_cc(message, [token[5]], env=env, _depth=_depth + 1)
+
+            if token[0] == 'block':
+                env = await self.execute_cc(message, token[1:], env=env, _depth=_depth + 1)
+
+            if token[0] == 'random':
+                env = await self.execute_cc(message, [random.choice(token[1:])], env=env, _depth=_depth + 1)
+
             if token[0] == 'speak':
                 await self.mbot.send_message(message.channel, token[1])
 
@@ -143,6 +254,11 @@ class CustomCommands(BasePlugin):
                 except Forbidden:
                     pass
 
+            elif token[0] == 'nop':
+                pass
+
+        return env
+
     @command(regex='^cc (.*?)(?: (.*?))?$', name='cc')
     async def run_cc(self, message, cmd, args=None):
         doc = await self.cc_db.find_one(
@@ -155,9 +271,8 @@ class CustomCommands(BasePlugin):
             )
 
         cmd_string = doc['cmd_string']
-        parsed = self.parse_cc(message, cmd_string, shlex.split(args) if args else [])
-
-        await self.execute_cc(message, parsed)
+        parsed = self.parse_cc(cmd_string, message, shlex.split(args) if args else [])
+        self.mbot.loop.create_task(self.execute_cc(message, parsed))
 
     @command(regex='^cc-add (.*?)$', name='cc-add', perms=32)
     async def add_cc(self, message, name):
@@ -183,12 +298,13 @@ class CustomCommands(BasePlugin):
 
         if cmd_string:
             try:
-                self.parse_cc(message, cmd_string.content, [])
+                self.parse_cc(cmd_string.content, message, [])
             except ParsingError as e:
                 return await self.mbot.send_message(
                     message.channel,
-                    '**There was an error while adding this command**\n'
-                    f'`{str(e)}`'
+                    '**There was an error while adding this command**\n\n'
+                    f'```{cmd_string.content}```\n'
+                    f':exclamation: **ERROR**\n```{str(e)}```'
                 )
 
             await self.cc_db.insert_one(
@@ -197,6 +313,6 @@ class CustomCommands(BasePlugin):
 
             await self.mbot.send_message(
                 message.channel,
-                f'**Successfully added command `{name}`!'
+                f'**Successfully added command `{name}`**!\n\n'
+                f'```{cmd_string.content}```'
             )
-
